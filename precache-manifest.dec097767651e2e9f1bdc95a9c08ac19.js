5a66642e633ceda9299ce890d6f688eb2674cdad
Larrygf02 <raulgf_02@hotmail.com>
2018-11-04 15:16:56 -0500

    Updates

---
+++ precache-manifest.dec097767651e2e9f1bdc95a9c08ac19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/search-book/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96c654edb34eda92d5f3",
+    "url": "/search-book/static/js/runtime~main.96c654ed.js"
+  },
+  {
+    "revision": "214ec71c37d9f8964d2d",
+    "url": "/search-book/static/js/main.214ec71c.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/search-book/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "214ec71c37d9f8964d2d",
+    "url": "/search-book/static/css/main.5c05771c.chunk.css"
+  },
+  {
+    "revision": "0043cda5d4ed7c1971f711bfa7eda20e",
+    "url": "/search-book/index.html"
+  }
+];
